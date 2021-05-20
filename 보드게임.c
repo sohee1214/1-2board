2d9e4099428e8495b7b62ae4ad24d1f61c3c3377
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2021-05-20 14:15:58 +0900

    중간마감처리(돈추가)

---
+++ 보드게임.c
@@ -0,0 +1,51 @@
+/*
+*	보드게임 만들기 21.05.13
+*	1. 전체 칸의 갯수 61개
+*	2. 플레이어가 가지고 있어야 할 것
+*		- 위치정보(말) : 0 부터 시작
+*		- 돈 : 5,000,000 부터 시작
+*		- 아이템 인벤토리 (나중에 가능하면)
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main(void)
+{
+	//칸의 최대갯수
+	int max_length = 61;
+
+	//플레이어의 위치
+	int position = 0;
+
+	//주사위
+	int dice;
+
+	int money = 5000000;
+
+	srand(time(0));
+	rand();
+
+	printf("현재위치 : %d, 가진 돈 : %d\n\n", position, money);
+
+
+	while (1)
+	{
+		getchar();		// 주사위를 던지기 위해서는 엔터키를 눌러야 한다.
+		//주사위 던지기 (1부터 6까지)
+		dice = rand() % 6 + 1;
+		position = position + dice;
+		printf("주사위를 던져서 %d가 나왔습니다.\n", dice);
+		printf("현재위치 : %d, 가진 돈 : %d\n\n", position, money);
+
+
+		if (position > 61)
+		{
+			printf("축하합니다. 목적지에 도달하였습니다.\n");
+			break;
+		}
+	} // while문 끝
+	printf("게임이 끝났습니다.");
+}

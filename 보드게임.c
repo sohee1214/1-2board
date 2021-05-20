536b48eef334d05641d2b62e496d255bb13abd3b
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2021-05-20 14:37:58 +0900

    말이 7번에 위치하면 3번으로 강제이동하는 기능 추가

--- 보드게임.c
+++ 보드게임.c
@@ -40,7 +40,15 @@ void main(void)
 		printf("주사위를 던져서 %d가 나왔습니다.\n", dice);
 		printf("현재위치 : %d, 가진 돈 : %d\n\n", position, money);
 
+		// 7번에 있을때는 3번으로 이동
+		if (position == 7)
+		{
+			printf("3번 위치로 이동합니다.\n");
+			position = 3;
+			printf("현재위치 : %d, 가진 돈 : %d\n\n", position, money);
+		}
 
+		// 61을 넘기면 목적지에 도착함
 		if (position > 61)
 		{
 			printf("축하합니다. 목적지에 도달하였습니다.\n");

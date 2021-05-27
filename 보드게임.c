e6effc9474cd3a747577ae729c01030dee29b167
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2021-05-27 14:36:41 +0900

    코드를 유연하게2

--- 보드게임.c
+++ 보드게임.c
@@ -39,7 +39,7 @@ void main(void)
 		getchar();		// 주사위를 던지기 위해서는 엔터키를 눌러야 한다.
 		//주사위 던지기 (1부터 6까지)
 		dice = rand() % 6 + 1;
-		position = position + dice;
+		position += dice;
 		printf("주사위를 던져서 %d가 나왔습니다.\n", dice);
 		printf("현재위치 : %d, 가진 돈 : %d\n\n", position, money);
 
@@ -62,7 +62,7 @@ void main(void)
 		{
 			printf("%d를 넘었기 때문에 무효처리 합니다.\n", MAX_LENGTH);
 			// 무효처리를 하기위한 코드
-			position = position - dice;
+			position -= dice;
 			printf("현재위치 : %d, 가진 돈 : %d\n\n", position, money);
 		}
 
@@ -82,5 +82,3 @@ void main(void)
 		}
 		////
 	} // while문 끝
-	printf("게임이 끝났습니다.");
-}

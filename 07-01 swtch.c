22ae3241ecc5326db9d8f06b4c59e3124a1f6ae0
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2021-06-03 13:38:29 +0900

    [문법설명] switch 바꾸기 전

---
+++ 07-01 swtch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main(void)
+{
+	int a = 3;
+
+	if (a == 1)
+	{
+		printf("c언어의 박지우");
+
+	}
+	else if (a == 2)
+	{
+		printf("선생님 안 사랑해");
+	}
+	else
+	{
+		printf("누가 나가는 소리를 내었는가");
+	}
+}

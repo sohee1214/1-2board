b20c0e0f582a4c70f2ccf47ff67083c4ba3825d9
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2021-06-10 14:26:28 +0900

    구구단2단 작성

---
+++ 07-03구구단.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(void)
+{
+	for (int i = 1; i <= 9; i++)
+	{
+		printf("2 X %d = %d\n", i, 2 * i);
+	}
+	/*
+	printf("2 X 2 = %d\n", 2*2);
+	printf("2 X 3 = %d\n", 2*3);
+	printf("2 X 4 = %d\n", 2*4);
+	printf("2 X 5 = %d\n", 2*5);
+	printf("2 X 6 = %d\n", 2*6);
+	printf("2 X 7 = %d\n", 2*7);
+	printf("2 X 8 = %d\n", 2*8);
+	printf("2 X 9 = %d\n", 2*9);
+	*/
+}

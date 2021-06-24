c86fbfc32702ff00b15a4e76d673f4d6334f1a06
YUN SOHEE <82165798+sohee1214@users.noreply.github.com>
2021-06-24 15:06:36 +0900

    [문법설명] break문

--- 07-03구구단.c
+++ 07-03구구단.c
@@ -1,31 +1,21 @@
-
 #include<stdio.h>
 
-	void main(void)
+void main(void)
+{
+	for (int dan = 2; dan <= 9; dan++) // 바깥쪽 for문
 	{
-		int dan = 2;
-		while (dan <= 9)
+		if (dan == 4)
 		{
-			int i = 1;
-			while (i <= 9)
-			{
-				printf("%d X %d = %d\n", dan, i, dan * i);
-				i++;
-			}
-			printf("\n");
-			dan++;
+			break;	// 바깥쪽 for문을 벗어난다
 		}
+		for (int i = 1; i <= 9; i++) // 안쪽 for문
+		{
+			if (i == 5)
+			{
+				break;	// 안쪽 for문을 벗어난다
+			}
+			printf("%d X %d = %d\n", dan, i, dan * i);
+		}
+		printf("\n");
 	}
-
-
-
-	/*
-	printf("2 X 2 = %d\n", 2*2);
-	printf("2 X 3 = %d\n", 2*3);
-	printf("2 X 4 = %d\n", 2*4);
-	printf("2 X 5 = %d\n", 2*5);
-	printf("2 X 6 = %d\n", 2*6);
-	printf("2 X 7 = %d\n", 2*7);
-	printf("2 X 8 = %d\n", 2*8);
-	printf("2 X 9 = %d\n", 2*9);
-	*/
+}

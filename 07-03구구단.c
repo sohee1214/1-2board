b95be8287279963e5ac15c0b189b2860ea23e1ee
YUN SOHEE <82165798+sohee1214@users.noreply.github.com>
2021-06-24 14:48:55 +0900

    [문법설명] 2중 while을 2중 for문으로

--- 07-03구구단.c
+++ 07-03구구단.c
@@ -1,17 +1,22 @@
-#include <stdio.h>
 
-void main(void)
-{
-	int dan = 2;
+#include<stdio.h>
 
-	int i = 1;
-	while (i <= 9)
+	void main(void)
 	{
-		printf("%d X %d = %d\n", dan, i, dan * i);
-		i++;
+		int dan = 2;
+		while (dan <= 9)
+		{
+			int i = 1;
+			while (i <= 9)
+			{
+				printf("%d X %d = %d\n", dan, i, dan * i);
+				i++;
+			}
+			printf("\n");
+			dan++;
+		}
 	}
 
-}
 
 
 	/*

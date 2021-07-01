7a914bf86c10b8ea27b76814266b13f510d4f089
YUN SOHEE <82165798+sohee1214@users.noreply.github.com>
2021-07-01 13:35:19 +0900

    Add files via upload

---
+++ 07-02 반복문.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(void)
+{
+    int num = 0;
+
+    while (1)
+    {
+        if (num == 5)
+        {
+            break;
+        }
+        printf("유한으로 즐겨\n");
+        num++;
+
+    }
+}

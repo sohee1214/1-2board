1aae1e59e45272bdba2adcc78660101c2624b1a7
YUN SOHEE <82165798+sohee1214@users.noreply.github.com>
2021-08-27 15:04:44 +0900

    배열의 기초

---
+++ 10-01 배열의 기초.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int arr[4] = { 10, 20, 30, 40 };
+
+	printf("%d\n", arr);
+
+	printf("%d\n", arr[0]);
+	printf("%d\n", arr[1]);
+	printf("%d\n", arr[2]);
+	printf("%d\n", arr[3]);
+}

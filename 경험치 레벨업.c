f9f4056eb14d555a3780704ed2cee06cff6ba5c1
YUN SOHEE <82165798+sohee1214@users.noreply.github.com>
2021-09-02 11:48:36 +0900

    함수 전방향선언

--- 경험치 레벨업.c
+++ 경험치 레벨업.c
@@ -1,13 +1,36 @@
-#include <stdio.h>
+include <stdio.h>
+
+void cal_grade(int score);
 
 void main(void)
 {
-	int score;
-	printf("점수를 입력하세요 : ");
-	scanf("%d", &score);
+	int C_score;
+	printf("씨언어 점수를 입력하세요 : ");
+	scanf("%d", &C_score);
 
+	cal_grade(C_score);
+
+
+	int java_score;
+	printf("자바 점수를 입력하세요 : ");
+	scanf("%d", &java_score);
+
+	cal_grade(java_score);
+
+
+	int CA_score;
+	printf("컴퓨터구조 점수를 입력하세요 : ");
+	scanf("%d", &CA_score);
+
+	cal_grade(CA_score);
+
+}
+
+// 입력받은 점수(score)에 대한 등급을 출력
+void cal_grade(int score)
+{
 	if (90 <= score && score <= 100)
-		printf("A입니다 \n")
+		printf("A입니다 \n");
 	else if (80 <= score)
 		printf("B입니다 \n");
 	else if (70 <= score)

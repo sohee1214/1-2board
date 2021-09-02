078ca026945d719155c7ab6a28c3fb3f57a22bc9
YUN SOHEE <82165798+sohee1214@users.noreply.github.com>
2021-09-02 12:17:35 +0900

    찐 평균값 추가

--- 경험치 레벨업.c
+++ 경험치 레벨업.c
@@ -1,29 +1,27 @@
-include <stdio.h>
+#include <stdio.h>
 
 void cal_grade(int score);
+double cal_average(int score_a, int score_b, int score_c);
 
 void main(void)
 {
 	int C_score;
 	printf("씨언어 점수를 입력하세요 : ");
 	scanf("%d", &C_score);
-
 	cal_grade(C_score);
-
 
 	int java_score;
 	printf("자바 점수를 입력하세요 : ");
 	scanf("%d", &java_score);
-
 	cal_grade(java_score);
-
 
 	int CA_score;
 	printf("컴퓨터구조 점수를 입력하세요 : ");
 	scanf("%d", &CA_score);
-
 	cal_grade(CA_score);
 
+	double average = cal_average(C_score, java_score, CA_score);
+	printf("세 과목의 평균은 %f입니다", average);
 }
 
 // 입력받은 점수(score)에 대한 등급을 출력
@@ -39,4 +37,10 @@ void cal_grade(int score)
 		printf("D입니다 \n");
 	else
 		printf("E입니다 \n");
-} //이다
+}
+
+// 세 과목의 평균을 반환
+double cal_average(int score_a, int score_b, int score_c)
+{
+	return (double)(score_a + score_b + score_c) / (double)3;
+}
